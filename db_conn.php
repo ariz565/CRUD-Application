80bd9419347da7fc555350540a8b18cd68958fdd
mohdariz.phy@gmail.com <mohdariz.phy@gmail.com>
2024-01-07 00:49:10 +0530

    adding files

---
+++ db_conn.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username="root";
+$password="";
+$dbname="php-crud";
+
+//create connection
+$conn = mysqli_connect($servername,$username,$password,$dbname);
+
+//check connection
+if(!$conn){
+    die("connection failed:" . mysqli_connect_error());
+}
